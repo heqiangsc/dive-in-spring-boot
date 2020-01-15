08181cb0d12727479441b42d69f0158cd1946070
heqiang <heqiang@kuainiugroup.com>
2020-01-15 11:30:02 +0800

    切面通知

--- hello-spring-webmvc/src/main/java/com/swift/hello/spring/webmvc/controller/HelloController.java
+++ hello-spring-webmvc/src/main/java/com/swift/hello/spring/webmvc/controller/HelloController.java
@@ -2,13 +2,14 @@ package com.swift.hello.spring.webmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
 public class HelloController {
 
     @RequestMapping("/")
-    public String index() {
+    public String index(@RequestParam int abc) {
         return "index";
     }
 }
--- hello-spring-webmvc/src/main/webapp/WEB-INF/jsp/index.jsp
+++ hello-spring-webmvc/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -1,1 +1,8 @@
-hello
+<jsp:root xmlns:jsp="http://java.sun.com/JSP/Page" version="2.0">
+    <body>
+    <message>${message}</message>
+    <acceptLanguage>${acceptLanguage}</acceptLanguage>
+    <jsessionId>${jsessionId}</jsessionId>
+
+    </body>
+</jsp:root>
